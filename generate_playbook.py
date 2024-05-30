918183f14029535cba9f7d9b8a8863102aae6290
Ryan Gniadek <ryan@gniadek.net>
2024-05-30 18:28:21 -0400

    Replace input with args

--- generate_playbook.py
+++ generate_playbook.py
@@ -16,10 +16,10 @@ if len(sys.argv) != 2:
 directory_path = sys.argv[1]
 
 # Count the number of playbooks generated
-shell_no_input = 0
-shell_input = 0
-python_no_input = 0
-python_input = 0
+shell_no_args = 0
+shell_args = 0
+python_no_args = 0
+python_args = 0
 
 # Loop through all files in the directory
 for filename in os.listdir(directory_path):
@@ -40,14 +40,14 @@ for filename in os.listdir(directory_path):
             script_contents = f.read()
             if ('?#' in script_contents or '?1' in script_contents) and filename.endswith(".sh"):
                 playbook_template = j2.get_template("sh_playbook_with_args.j2")
-                shell_input += 1
+                shell_args += 1
             elif filename.endswith(".sh"):
                 playbook_template = j2.get_template("sh_playbook.j2")
-                shell_no_input += 1
-            # TODO: Add support for Python scripts with input
+                shell_no_args += 1
+            # TODO: Add support for Python scripts with args
             elif filename.endswith(".py"):
                 playbook_template = j2.get_template("py_playbook.j2")
-                python_no_input += 1
+                python_no_args += 1
 
         # Create an Ansible playbook that executes the script
         playbook_path = os.path.join(script_directory, "playbook.yml")
@@ -58,9 +58,9 @@ print("Hi {}, happy automating!".format(
     os.getlogin(), ))
 print()
 print("Playbooks generated:")
-print("Shell scripts converted (without input): {}".format(shell_no_input))
-print("Shell scripts converted (with input): {}".format(shell_input))
-print("Python scripts converted (without input): {}".format(python_no_input))
-print("Python scripts converted (with input): {}".format(python_input))
+print("Shell scripts converted (without args): {}".format(shell_no_args))
+print("Shell scripts converted (with args): {}".format(shell_args))
+print("Python scripts converted (without args): {}".format(python_no_args))
+print("Python scripts converted (with args): {}".format(python_args))
 print("Total playbooks generated: {}".format(
-    shell_no_input + shell_input + python_no_input + python_input))
+    shell_no_args + shell_args + python_no_args + python_args))

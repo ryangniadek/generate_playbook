4506806c16281a0801b32f8d17eb2583bcaa035f
Ryan Gniadek <ryan@gniadek.net>
2024-05-30 18:25:43 -0400

    Scripts with input

--- generate_playbook.py
+++ generate_playbook.py
@@ -16,8 +16,10 @@ if len(sys.argv) != 2:
 directory_path = sys.argv[1]
 
 # Count the number of playbooks generated
-shell = 0
-python = 0
+shell_no_input = 0
+shell_input = 0
+python_no_input = 0
+python_input = 0
 
 # Loop through all files in the directory
 for filename in os.listdir(directory_path):
@@ -38,21 +40,27 @@ for filename in os.listdir(directory_path):
             script_contents = f.read()
             if ('?#' in script_contents or '?1' in script_contents) and filename.endswith(".sh"):
                 playbook_template = j2.get_template("sh_playbook_with_args.j2")
-                shell += 1
+                shell_input += 1
             elif filename.endswith(".sh"):
                 playbook_template = j2.get_template("sh_playbook.j2")
-                shell += 1
+                shell_no_input += 1
+            # TODO: Add support for Python scripts with input
             elif filename.endswith(".py"):
                 playbook_template = j2.get_template("py_playbook.j2")
-                python += 1
+                python_no_input += 1
 
         # Create an Ansible playbook that executes the script
         playbook_path = os.path.join(script_directory, "playbook.yml")
         with open(playbook_path, "w") as f:
             f.write(playbook_template.render(filename=filename))
 
-print("Hi {}, you've generated {} playbooks. Happy automating!".format(
-    os.getlogin(), shell + python))
+print("Hi {}, happy automating!".format(
+    os.getlogin(), ))
 print()
-print("Shell scripts converted: {}".format(shell))
-print("Python scripts converted: {}".format(python))
+print("Playbooks generated:")
+print("Shell scripts converted (without input): {}".format(shell_no_input))
+print("Shell scripts converted (with input): {}".format(shell_input))
+print("Python scripts converted (without input): {}".format(python_no_input))
+print("Python scripts converted (with input): {}".format(python_input))
+print("Total playbooks generated: {}".format(
+    shell_no_input + shell_input + python_no_input + python_input))

3da792f6372d90ce569a12cc3d09920f58ff193e
Ryan Gniadek <ryan@gniadek.net>
2024-06-04 12:52:58 -0400

    Update generate_playbook.py
    Support shell script with args

--- generate_playbook.py
+++ generate_playbook.py
@@ -3,10 +3,10 @@
 import os
 import shutil
 import sys
-from jinja2 import Template, Environment
+from jinja2 import Environment, FileSystemLoader
 
 # Load the jinja environment
-j2 = Environment()
+j2 = Environment(loader=FileSystemLoader(searchpath="./templates"))
 
 # Get the directory path from the command-line argument
 if len(sys.argv) != 2:
@@ -25,29 +25,33 @@ python_args = 0
 for filename in os.listdir(directory_path):
     # Check if the file is a shell script
     if filename.endswith((".sh", ".py")):
+        # Determine script path
+        script_path = os.path.join(directory_path, filename)
+
+        # Decide which playbook template to use
+        playbook_template = None
+        with open(script_path, 'r') as f:
+            script_contents = f.read()
+            if ('$#' in script_contents or '$1' in script_contents) and filename.endswith(".sh"):
+                playbook_template = j2.get_template("shell_playbook_with_args.j2")
+                shell_args += 1
+            elif filename.endswith(".sh"):
+                playbook_template = j2.get_template("shell_playbook.j2")
+                shell_no_args += 1
+            # # TODO: Add support for Python scripts with args
+            elif filename.endswith(".py"):
+                playbook_template = j2.get_template("python_playbook.j2")
+                python_no_args += 1
+            else:
+                continue
+
         # Create a subdirectory for the script
         script_directory = os.path.join(
             directory_path, filename.rsplit('.', 1)[0])
         os.makedirs(script_directory, exist_ok=True)
 
         # Move the script into its subdirectory
-        script_path = os.path.join(directory_path, filename)
         shutil.move(script_path, script_directory)
-
-        # Decide which playbook template to use
-        playbook_template = None
-        with open(script_path, 'r') as f:
-            script_contents = f.read()
-            if ('?#' in script_contents or '?1' in script_contents) and filename.endswith(".sh"):
-                playbook_template = j2.get_template("sh_playbook_with_args.j2")
-                shell_args += 1
-            elif filename.endswith(".sh"):
-                playbook_template = j2.get_template("sh_playbook.j2")
-                shell_no_args += 1
-            # TODO: Add support for Python scripts with args
-            elif filename.endswith(".py"):
-                playbook_template = j2.get_template("py_playbook.j2")
-                python_no_args += 1
 
         # Create an Ansible playbook that executes the script
         playbook_path = os.path.join(script_directory, "playbook.yml")
